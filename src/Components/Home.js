338dfd1701e0afbb886ce844b7058e8781d35b16
LEO-Malay <malaybhavsar.290@gmail.com>
2021-04-05 22:16:10 +0530

    Deploy script & Home Page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../CSS/home.css";
 
 const Home = () => {
     return (
         <div>
-            <p>Test 1</p>
-            <p>Test 1</p>
-            <p>Test 1</p>
-            <p>Test 1</p>
-            <p>Test 1</p>
+            <p className="PageTitle">Welcome to Leo-Movies</p>
+            <center>
+                <p className="PageSubTitle">
+                    Developed by Malay Bhavsar to showcast his all time hot
+                    favourite Movies!
+                    <br /> Here You will find many movies that have been famous
+                    among many people. If you think that there's some movie
+                    missing then please feel free to contact me via Email given
+                    in the footer section.
+                    <br />
+                </p>
+            </center>
+            <Link to={"/movie"}>
+                <button className="MovieBtn">Go to Movies</button>
+            </Link>
         </div>
     );
 };
